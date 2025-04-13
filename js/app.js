1a8f2a687b8a99ec626d2c696ecfb50f87f24007
BoburDv <exnessbobur@gmail.com>
2025-04-13 14:30:12 +0100

    LoginPage

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 window.addEventListener("load", function(){
-    const user = JSON.parse(localStorage.getItem("user"));
+    const user = JSON.parse(localStorage.getItem("user"))
     if(user){
         window.location.href = "/index.html";
     }
-});
+})
 
 const elForm = document.getElementById("form");
 
@@ -18,30 +18,30 @@ elForm.addEventListener("submit", function(e){
     const checker = validator(result);
 
     if (checker) {
-        const toast = document.getElementById("toast-warning");
-        const toastText = document.getElementById("toast-warning-message");
-        toastText.textContent = checker.massage;
-        toast.classList.remove("hidden");
-        toast.classList.add("flex");
+        const toast = document.getElementById("toast-warning")
+        const toastText = document.getElementById("toast-warning-message")
+        toastText.textContent = checker.massage
+        toast.classList.remove("hidden")
+        toast.classList.add("flex")
         setTimeout(() => {
-            toast.classList.add("hidden");
-            toast.classList.remove("flex");
+            toast.classList.add("hidden")
+            toast.classList.remove("flex")
         }, 3000);
         e.target[checker.target].focus();
     } else {
         login(result);
     }
-});
+})
 
 function validator(obj){
-    if(obj.username.trim() === ""){
+    if(obj.username.trim() == ""){
         return{
             target: "username",
             massage: "Foydalanuvchi ismi bo'sh bo'lishi mumkin emas"
         };
     }
 
-    if(obj.password.trim() === ""){
+    if(obj.password.trim() == ""){
         return{
             target: "password",
             massage: "Foydalanuvchi paroli bo'sh bo'lishi mumkin emas"
@@ -54,7 +54,7 @@ function login(userData){
     const spinner = document.querySelector('button svg');
     spinner.style.display = "inline-block";
 
-    fetch("https://json-api.uz/api/project/fn37/auth/login", {
+    fetch("https://json-api.uz/api/project/fn37/auth/login",{
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -63,47 +63,47 @@ function login(userData){
     })
     .then((res) => res.json())
     .then((res) => {
-        if (res.token) {
-            localStorage.setItem("user", JSON.stringify(res.user));
+        if (res.token){
+            localStorage.setItem("user", JSON.stringify(res.user))
 
-            const toast = document.getElementById("toast-success");
-            const toastText = document.getElementById("toast-success-message");
-            toastText.textContent = "Kirish muvaffaqiyatli bo‘ldi";
-            toast.classList.remove("hidden");
-            toast.classList.add("flex");
+            const toast = document.getElementById("toast-success")
+            const toastText = document.getElementById("toast-success-message")
+            toastText.textContent = "Kirish muvaffaqiyatli"
+            toast.classList.remove("hidden")
+            toast.classList.add("flex")
 
             setTimeout(() => {
-                toast.classList.add("hidden");
-                toast.classList.remove("flex");
-                window.location.href = "/index.html";
+                toast.classList.add("hidden")
+                toast.classList.remove("flex")
+                window.location.href = "/index.html"
             }, 2000);
         } else {
-            const toast = document.getElementById("toast-danger");
-            const toastText = document.querySelector("#toast-danger .ms-3");
-            toastText.textContent = "Login yoki parol noto‘g‘ri!";
-            toast.classList.remove("hidden");
-            toast.classList.add("flex");
+            const toast = document.getElementById("toast-danger")
+            const toastText = document.querySelector("#toast-danger .ms-3")
+            toastText.textContent = "Login yoki parol noto'g'ri"
+            toast.classList.remove("hidden")
+            toast.classList.add("flex")
 
             setTimeout(() => {
-                toast.classList.add("hidden");
-                toast.classList.remove("flex");
+                toast.classList.add("hidden")
+                toast.classList.remove("flex")
             }, 3000);
         }
     })
     .catch((error) => {
         console.log(error);
-        const toast = document.getElementById("toast-danger");
-        const toastText = document.querySelector("#toast-danger .ms-3");
-        toastText.textContent = "Serverda xatolik yuz berdi";
-        toast.classList.remove("hidden");
-        toast.classList.add("flex");
+        const toast = document.getElementById("toast-danger")
+        const toastText = document.querySelector("#toast-danger .ms-3")
+        toastText.textContent = "server xato"
+        toast.classList.remove("hidden")
+        toast.classList.add("flex")
 
         setTimeout(() => {
-            toast.classList.add("hidden");
-            toast.classList.remove("flex");
-        }, 3000);
+            toast.classList.add("hidden")
+            toast.classList.remove("flex")
+        }, 2000)
     })
-    .finally(() => {
-        spinner.style.display = "none";
+    .finally(() =>{
+        spinner.style.display = "none"
     });
 }
